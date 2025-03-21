4f5355229d1ecc74036bfb9b5ad86e233d05aef0
Anga205 <github@angadbhalla.com>
2025-03-21 16:21:12 +0530

    modified chart colours

--- src/components/solidjs/SiteActivity.tsx
+++ src/components/solidjs/SiteActivity.tsx
@@ -2,6 +2,7 @@ import type { Component } from 'solid-js';
 import { onMount } from 'solid-js';
 import { Chart, Title, Tooltip, Colors } from 'chart.js'
 import { Line } from 'solid-chartjs'
+import { color } from 'chart.js/helpers';
 
 const SiteActivity: Component = () => {
     /**
@@ -9,27 +10,49 @@ const SiteActivity: Component = () => {
      * otherwise you will have the most primitive UI
      */
     onMount(() => {
-      Chart.register(Title, Tooltip, Colors)
-  })
+        Chart.register(Title, Tooltip, Colors)
+    })
 
-  const chartData = {
-      labels: ['Mar 13', 'Mar 14', 'Mar 15', 'Mar 16', 'Mar 17', 'Mar 18', 'Mar 19'],
-      datasets: [
-          {
-              data: [124, 83, 46, 36, 64, 54, 70],
-          },
-      ],
-  }
-  const chartOptions = {
-      responsive: true,
-      maintainAspectRatio: false,
-  }
+    const chartData = {
+        labels: ['Mar 13', 'Mar 14', 'Mar 15', 'Mar 16', 'Mar 17', 'Mar 18', 'Mar 19'],
+        datasets: [
+            {
+                label: 'Database Reads',
+                data: [124, 83, 46, 36, 64, 54, 70],
+                borderColor: 'rgba(75, 75, 192, 1)',
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderWidth: 1.5,
+            },
+        ],
+    }
+    const chartOptions = {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            x: {
+                grid: {
+                    color: 'rgba(255, 255, 255, 0.1)',
+                },
+                ticks: {
+                    color: 'rgba(255, 255, 255, 0.3)',
+                },
+            },
+            y: {
+                grid: {
+                    color: 'rgba(255, 255, 255, 0.1)',
+                },
+                ticks: {
+                    color: 'rgba(255, 255, 255, 0.3)',
+                },
+            }
+        }
+    }
 
-  return (
-      <div class="w-full h-full pb-[2vh]">
-          <Line data={chartData} options={chartOptions} />
-      </div>
-  )
+    return (
+        <div class="w-full h-full pb-[2vh]">
+            <Line data={chartData} options={chartOptions} />
+        </div>
+    )
 }
 
 export default SiteActivity
