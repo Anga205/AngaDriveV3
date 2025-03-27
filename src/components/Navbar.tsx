29458612225f9a80b69664321091b14dcb9af4f9
Anga205 <github@angadbhalla.com>
2025-03-27 20:18:37 +0530

    Migrate everything to solidjs

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,47 @@
+import type { Component } from 'solid-js'
+import { CollectionSVG, FileSVG, GitHubSVG, HomeSVG, UserSVG } from '../assets/SvgFiles'
+
+const Navbar: Component = () => {
+    return (
+        <div class="bg-[#161717] h-screen w-[5.2%] border-white md:flex hidden flex-col items-center">
+            <div class="p-[20%] h-[10vh]">
+                <img src="/anga.svg" alt="Anga Logo" class="w-full h-auto" />
+            </div>
+
+            <div class="flex w-full mt-[10%] justify-end">
+                <div class="flex w-5/6 bg-black hover:bg-[#161717] rounded-l-2xl">
+                    <div class="p-[20%] h-full aspect-square">
+                        <HomeSVG />
+                    </div>
+                </div>
+            </div>
+            <div class="flex w-full mt-[5%] justify-end">
+                <div class="flex w-5/6 bg-[#161717] hover:bg-black rounded-l-2xl">
+                    <div class="p-[20%]">
+                        <FileSVG />
+                    </div>
+                </div>
+            </div>
+            <div class="flex w-full mt-[5%] justify-end">
+                <div class="flex w-5/6 bg-[#161717] hover:bg-black rounded-l-2xl">
+                    <div class="p-[20%]">
+                        <CollectionSVG />
+                    </div>
+                </div>
+            </div>
+            <div class="flex w-full mt-[5%] justify-end">
+                <div class="flex w-5/6 bg-[#161717] hover:bg-black rounded-l-2xl">
+                    <div class="p-[20%]">
+                        <GitHubSVG />
+                    </div>
+                </div>
+            </div>
+            <div class="flex-grow" />
+            <div class="w-full p-[32%]">
+                <UserSVG />
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
