9b4a84c40be3d0f5d7b6600e923619557a9cf7e1
Anga205 <github@angadbhalla.com>
2025-03-29 20:52:28 +0530

    added a page for gallery

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,9 @@
 import type { Component } from 'solid-js'
 import { CollectionSVG, FileSVG, GitHubSVG, HomeSVG, UserSVG } from '../assets/SvgFiles'
 
-const Navbar: Component = () => {
+const Navbar: Component<{ CurrentPage?: string }> = (props) => {
+    const currentPage = props.CurrentPage || "Home";
+
     return (
         <div class="bg-[#161717] h-screen w-[4.5vw] border-white flex flex-col items-center">
             <div class="p-[20%] h-[10vh]">
@@ -9,28 +11,28 @@ const Navbar: Component = () => {
             </div>
 
             <div class="flex w-full mt-[10%] justify-end">
-                <div class="flex w-5/6 bg-black hover:bg-[#161717] rounded-l-2xl">
+                <div class={`flex w-5/6 ${currentPage === "Home" ? "bg-black" : "bg-[#161717] hover:bg-black"} rounded-l-2xl`}>
                     <div class="p-[20%] h-full aspect-square">
                         <HomeSVG />
                     </div>
                 </div>
             </div>
             <div class="flex w-full mt-[5%] justify-end">
-                <div class="flex w-5/6 bg-[#161717] hover:bg-black rounded-l-2xl">
+                <div class={`flex w-5/6 ${currentPage === "Files" ? "bg-black" : "bg-[#161717] hover:bg-black"} rounded-l-2xl`}>
                     <div class="p-[20%]">
                         <FileSVG />
                     </div>
                 </div>
             </div>
             <div class="flex w-full mt-[5%] justify-end">
-                <div class="flex w-5/6 bg-[#161717] hover:bg-black rounded-l-2xl">
+                <div class={`flex w-5/6 ${currentPage === "Collections" ? "bg-black" : "bg-[#161717] hover:bg-black"} rounded-l-2xl`}>
                     <div class="p-[20%]">
                         <CollectionSVG />
                     </div>
                 </div>
             </div>
             <div class="flex w-full mt-[5%] justify-end">
-                <div class="flex w-5/6 bg-[#161717] hover:bg-black rounded-l-2xl">
+                <div class={`flex w-5/6 ${currentPage === "GitHub" ? "bg-black" : "bg-[#161717] hover:bg-black"} rounded-l-2xl`}>
                     <div class="p-[20%]">
                         <GitHubSVG />
                     </div>
---
+++ src/pages/MyDrive.tsx
@@ -0,0 +1,11 @@
+import { DesktopTemplate } from "../components/Template"
+
+const DesktopDrive = () => {
+    return (
+        <DesktopTemplate>
+            <p>My Drive</p>
+        </DesktopTemplate>
+    )
+}
+
+export {DesktopDrive}
